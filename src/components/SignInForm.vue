9ead93a61ee97b8129f534b0b07495cf3a9ef5e6
Kevin Brinach <kevinbrinach@gmail.com>
2020-10-26 12:40:51 -0300

    finished

--- src/components/SignInForm.vue
+++ src/components/SignInForm.vue
@@ -1,50 +1,56 @@
 <template>
   <div>
-    <h2 class="greenish-blue-text mt-5 mb-4 bolder font-26">
-      Ingresa a tu cuenta
-    </h2>
-    <error-message :msg="errorMessage" :bold="false" v-if="errorMessage" />
-    <form @submit.prevent="handleSubmit">
-      <div class="form-group">
-        <label class="darkgray-text bold font-13" for="email">Email</label>
-        <input
-          type="email"
-          class="form-control"
-          id="email"
-          placeholder="Ej: usuario@mail.com"
-          v-model.lazy="email"
-        />
-      </div>
-      <error-message msg="Necesitamos tu email." v-if="errorEmail" />
-      <div class="form-group">
-        <label class="darkgray-text bold font-13" for="password"
-          >Contraseña</label
+    <template v-if="!loggedIn">
+      <h2 class="greenish-blue-text mt-5 mb-4 bolder font-26">
+        Ingresa a tu cuenta
+      </h2>
+      <error-message :msg="errorMessage" :bold="false" v-if="errorMessage" />
+      <form @submit.prevent="handleSubmit">
+        <div class="form-group">
+          <label class="darkgray-text bold font-13" for="email">Email</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            placeholder="Ej: usuario@mail.com"
+            v-model.lazy="email"
+          />
+        </div>
+        <error-message msg="Necesitamos tu email." v-if="errorEmail" />
+        <div class="form-group">
+          <label class="darkgray-text bold font-13" for="password"
+            >Contraseña</label
+          >
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            placeholder="Escribe tu contraseña"
+            v-model="password"
+          />
+        </div>
+        <error-message msg="Necesitamos tu contraseña." v-if="errorPass" />
+        <div class="pl-0 mt-2 mb-2 link-decoration">
+          <a class="forgot-pass">¿Olvidaste tu contraseña?</a>
+        </div>
+        <button
+          class="btn btn-action-2 btn-lg btn-block mt-3 btn-login font-13"
+          type="submit"
         >
-        <input
-          type="password"
-          class="form-control"
-          id="password"
-          placeholder="Escribe tu contraseña"
-          v-model="password"
-        />
-      </div>
-      <error-message msg="Necesitamos tu contraseña." v-if="errorPass" />
-      <div class="pl-0 mt-2 mb-2 link-decoration">
-        <a class="forgot-pass">¿Olvidaste tu contraseña?</a>
-      </div>
-      <button
-        class="btn btn-action-2 btn-lg btn-block mt-3 btn-login font-13"
-        type="submit"
-      >
-        Ingresar a mi cuenta
-      </button>
-    </form>
+          Ingresar a mi cuenta
+        </button>
+      </form>
+    </template>
+    <div class="loggedIn-container container">
+      <h5 class="greenish-blue-text">Estás logeado</h5>
+      <code>{{ bearerToken }}</code>
+    </div>
   </div>
 </template>
 
 <script>
 import ErrorMessage from "./ErrorMessage.vue";
-import { mapActions, mapGetters } from "vuex";
+import { mapActions, mapGetters, mapState } from "vuex";
 
 export default {
   name: "SignInForm",
@@ -57,10 +63,12 @@ export default {
       password: "",
       errorEmail: false,
       errorPass: false,
+      loggedIn: true,
     };
   },
   computed: {
     ...mapGetters("user", ["errorMessage"]),
+    ...mapState("user", ["bearerToken"]),
   },
   methods: {
     ...mapActions("user", ["Login"]),
@@ -78,8 +86,20 @@ export default {
       this.Login({
         Username: this.email,
         Password: this.password,
+      }).then(() => {
+        if (this.bearerToken) {
+          this.loggedIn = true;
+        }
       });
     },
   },
 };
 </script>
+
+<style scoped>
+.loggedIn-container {
+  position: absolute;
+  bottom: 50%;
+  text-align: center;
+}
+</style>
